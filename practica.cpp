d4955b5d76d308245e9c31c7b75ad86270bfb6c9
alejotheprog <alejandro.cordoba.bodhert@gmail.com>
2016-05-22 20:36:05 -0500

    lectura ready

--- practica.cpp
+++ practica.cpp
@@ -1,40 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+struct Coordenadas
+{
+  char coordinatex [30];
+  char coordinatey [30];
+};
+
+
 const int MAXN = 16000;
+const int MAXDIS = 30;
+const int INF = 1 << 30;
+
 bool visited[MAXN];
-// typedef pair <int, int> edge;
-// typedef pair<int, pair <double,double> > node_cordenadas;
+
 typedef pair <int, int> edge;
-vector < pair<int, int> > g[MAXN]; // para trabajaar mañan
-map<int, string> coordinateXmap;
-map<int, string> coordinateYmap;
+vector < pair<int, int> > g[MAXN];
+map <int, Coordenadas> cordinates;
+
 int d[MAXN];
 int p[MAXN];
+int dis[MAXDIS][MAXDIS];
 
-void printCoordenadasX()
+
+void setContainers()
 {
-  for(map <int, string>::iterator it = coordinateXmap.begin();
-  it != coordinateXmap.end(); ++it)
+  for(int i = 0; i < MAXN; ++i)
   {
-    cout << it -> first << ": " << it -> second<< endl;
+    d[i] = INF;
+    p[i] = -1;
+    g[i].clear();
+  }
+
+  for (int i = 0; i < MAXDIS; ++i)
+    for(int j = 0; j < MAXDIS; ++j)
+      dis[i][j] = INF;
+}
+
+void printConexAndWeigth()
+{
+  for(int i = 0; i < MAXN; ++i )
+  {
+    if(g[i].size() != 0){
+      cout << "nodo : " << i << " va a: (nodo, distancia) " << endl;
+      for(int j = 0; j < g[i].size(); ++j)
+      {
+        cout << g[i][j].first << " ";
+        cout << g[i][j].second << " ";
+        cout << endl;
+      }
+    }
   }
 }
 
-
-void printCoordenadasY()
+void printCordenadas()
 {
-  for(map <int, string>::iterator it = coordinateYmap.begin();
-  it != coordinateYmap.end(); ++it)
-  {
-    cout << it -> first << ": " << it -> second << endl;
-  }
+
+for(map <int,Coordenadas>:: iterator it = cordinates.begin(); it !=
+ cordinates.end(); ++it)
+ {
+   cout <<" nodo: " << it -> first << " x = " << it -> second.coordinatex << " y = "
+   << it -> second.coordinatey << endl;
+ }
 }
 
-void storeCordenada(int vertex, string coordinateX,string coordinateY)
+void storeCordenada(int vertex, char *coordinateX,char *coordinateY)
 {
-  coordinateXmap[vertex] = coordinateX;
-  coordinateYmap[vertex] = coordinateY;
+  struct Coordenadas c;
+
+//cout << vertex << " " << c.coordinatex << " " << c.coordinatey << endl;
+  strcpy(c.coordinatex,coordinateX);
+  strcpy(c.coordinatey,coordinateY);
+
+  cordinates[vertex] = c;
+
+  //cout << vertex << " " << c.coordinatex << " " << c.coordinatey << endl;
 }
 
 void saveGraph(int vertex1, int vertex2,int weigth)
@@ -49,7 +90,7 @@ void readFirstandStore()
   int vertex,vertex1,vertex2,weigth;
   char coordinateX[20],coordinateY[20];
 
-  getline(cin,ignore); cout << ignore;
+  getline(cin,ignore); cout << ignore << endl;
   //getline(cin,ignore); cout << ignore;
   while(scanf("%i %s %s",&vertex, coordinateX, coordinateY))
   {
@@ -58,7 +99,7 @@ void readFirstandStore()
     storeCordenada(vertex, coordinateX,coordinateY);
   }
   //getline(cin,ignore); cout << ignore;
-  getline(cin,ignore);cout << ignore;
+  getline(cin,ignore);cout << ignore << endl;
 
   while(scanf("%i %i %i",&vertex1,&vertex2,&weigth) != EOF)
   {
@@ -72,8 +113,9 @@ void readFirstandStore()
 int main()
 {
   std::ios::sync_with_stdio(true);
+  setContainers();
   readFirstandStore();
-  printCoordenadasX();
-  printCoordenadasY();
+  //printCordenadas();
+  //printConexAndWeigth();
   return 0;
 }
